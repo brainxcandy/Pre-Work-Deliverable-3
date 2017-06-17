d906cc0f277df26f9ef551ec85cd618362c48085
brainxcandy <amberb.uxdesignpro@gmail.com>
2017-06-17 11:01:19 -0400

    version three

---
+++ Hoey.Portfolio/.js
@@ -0,0 +1,7 @@
+<script>
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Thank You.";
+}
+<p id="demo">Questions For Me</p>
+<button type="button" onclick="myFunction()">Submit</button>
